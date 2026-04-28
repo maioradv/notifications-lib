aaea88f045662b3e0ede53bf83fde35c4a6723c8
Roberto Colabene <robertocolabene@maioradv.com>
2026-04-28 10:23:37 +0200

    dev

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,10 @@ export enum ApiVersion {
   Unstable = 'unstable'
 }
 
+export enum ApiHost {
+  main = 'api.notifications.maior.cloud'
+}
+
 export const LATEST_API_VERSION = ApiVersion.March26
 export const SUPPORTED_API_VERSIONS = [
   LATEST_API_VERSION,
