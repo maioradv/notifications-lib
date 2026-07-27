f87b0678338dec8ed45f1f36b888cdecec9634d3
Roberto Colabene <robertocolabene@maioradv.com>
2026-07-27 12:39:18 +0200

    dev

--- src/me/images.ts
+++ src/me/images.ts
@@ -7,7 +7,7 @@ export default class Images extends ApiModule {
    * @requires TenantID - Set Workspace ID with {@link ApiClient.setTenantID}
    */
   create(args:CreateOwnChannelDto): Promise<Image> {
-    return this._call('post','/images',args,{
+    return this._call('post','/me/images',args,{
       headers:{
         'Content-Type':'multipart/form-data'
       }
@@ -18,7 +18,7 @@ export default class Images extends ApiModule {
    * @requires TenantID - Set Workspace ID with {@link ApiClient.setTenantID}
    */
   RNCreate(formData:any): Promise<Image> {
-    return this._call('post',`/images`,formData,{
+    return this._call('post',`/me/images`,formData,{
       headers:{
         'Content-Type':'multipart/form-data'
       }
@@ -29,27 +29,27 @@ export default class Images extends ApiModule {
    * @requires TenantID - Set Workspace ID with {@link ApiClient.setTenantID}
    */
   findAll(args:QueryOwnImageDto = {}): Promise<PaginatedDto<Image>> {
-    return this._call('get','/images',queryParams(args))
+    return this._call('get','/me/images',queryParams(args))
   } 
 
   /**
    * @requires TenantID - Set Workspace ID with {@link ApiClient.setTenantID}
    */
   findOne(id:number): Promise<Image> {
-    return this._call('get',`/images/${id}`)
+    return this._call('get',`/me/images/${id}`)
   }
 
   /**
    * @requires TenantID - Set Workspace ID with {@link ApiClient.setTenantID}
    */
   remove(id:number): Promise<Image> {
-    return this._call('delete',`/images/${id}`)
+    return this._call('delete',`/me/images/${id}`)
   }
 
   /**
    * @requires TenantID - Set Workspace ID with {@link ApiClient.setTenantID}
    */
   removeMany(ids:number[]): Promise<BulkResponse> {
-    return this._call('delete',`/images`,{ids})
+    return this._call('delete',`/me/images`,{ids})
   }
 }
